487de86568f0b17a6efb8d7cca922fb784250952
JumpyJay <iuswtric1909@gmail.com>
2025-10-19 20:48:00 +0800

    Finish server implementation, broadcast changes to all client successfully

--- src/modules/collaboration/session.server.ts
+++ src/modules/collaboration/session.server.ts
@@ -1,10 +1,13 @@
 const io = require("socket.io")(3001, {
   cors: {
+    // remember to change cors when deploy
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket: any) => {
-  console.log("a user connected");
+  socket.on("send-code", (delta: any) => {
+    socket.broadcast.emit("receive-code", delta);
+  });
 });
